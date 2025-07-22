a4bdc69c6f21d2432064aa97111b2276533e283f
CLBonner <clawrence.bonner@gmail.com>
2025-07-22 22:15:06 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,4 @@
-# entry point for running the close pass data logger
+from closepass import closepass
 
-from closepass import *
-
-waitForGPSReady()
-
-while True:
-    distance = getDistance()
-    if (isClosePass(distance)):
-        logClosePass(distance)
+logger = closepass()
+logger.run()
